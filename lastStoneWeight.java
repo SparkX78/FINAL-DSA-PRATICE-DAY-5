539f53fd994cc6bd0de690767a405820339a3c99
Debadipta Sahu <debssahu123@gmail.com>
2025-12-19 19:21:53 +0530

    added last stone weight

---
+++ lastStoneWeight.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class lastStoneWeight {
+    public static int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int stone : stones){
+            pq.add(stone);
+        }
+        while(pq.size() > 1){
+            int y = pq.poll();
+            int x = pq.poll();
+            if(x != y){
+                pq.add(y-x);
+            }
+        }
+        return pq.isEmpty()?0 : pq.poll();
+
+    }
+
+    public static void main(String[] args){
+        int[] stones = {2,7,4,1,8,1};
+        System.out.println(lastStoneWeight(stones));
+    }
+}
